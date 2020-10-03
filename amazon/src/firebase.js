13cb2832ef3e52a276a37e0892c71940723d7859
parthshirke <shirkeparth3333@gmail.com>
2020-10-03 10:01:45 +0530

    intial comment

---
+++ amazon/src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyChyAmPMMcbfVwunieASJXuN-y-LQiQXP4",
+    authDomain: "clone-c12c1.firebaseapp.com",
+    databaseURL: "https://clone-c12c1.firebaseio.com",
+    projectId: "clone-c12c1",
+    storageBucket: "clone-c12c1.appspot.com",
+    messagingSenderId: "70233909853",
+    appId: "1:70233909853:web:a43b03e419a8b7eaa93bb4",
+    measurementId: "G-V9KVXQ15C0"
+  };
